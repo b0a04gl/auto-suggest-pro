76584418bc564b395f2583d397d2487e647b31c4
b0a04gl <balaji.foo404@gmail.com>
2023-10-24 22:56:06 +0530

    added Worker runnable

---
+++ aggregator/src/test/java/org/personal/gallery/aggregator/service/core/models/WordFrequencyTest.java
@@ -0,0 +1,10 @@
+package org.personal.gallery.aggregator.service.core.models;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Objects;
+
+@ExtendWith(MockitoExtension.class)
+public class WordFrequencyTest {
+}
