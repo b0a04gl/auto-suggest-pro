1cc063aa152f6acc73c1104a7a8ac7abee0175bb
b0a04gl <balaji.foo404@gmail.com>
2023-10-25 08:40:25 +0530

    added required models

---
+++ suggester/src/main/java/org/personal/gallery/suggester/service/core/model/WordFrequency.java
@@ -0,0 +1,43 @@
+package org.personal.gallery.suggester.service.core.model;
+
+import java.util.Objects;
+
+
+public class WordFrequency {
+
+    private final String word;
+    private Integer frequency;
+
+    public String getWord() {
+        return word;
+    }
+
+    public Integer getFrequency() {
+        return frequency;
+    }
+
+    public WordFrequency(String wordString, Integer frequency) {
+        this.word = wordString;
+        this.frequency = frequency;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof WordFrequency that)) return false;
+        return Objects.equals(word, that.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word, frequency);
+    }
+
+    public static WordFrequency from(StringBuilder word, Integer frequency){
+        return  new WordFrequency(word.toString(),frequency);
+    }
+
+    public void addFrequency(int frequency) {
+        this.frequency+=frequency;
+    }
+}
